950ee98d7b0cf7ad3aa7b6e13f52e0cd6f64c84f
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2026-03-21 04:21:17 +0100

    add new config and use utf8 strings

--- ReDalamud.Standalone/Windows/ConfigWindow.cs
+++ ReDalamud.Standalone/Windows/ConfigWindow.cs
@@ -12,20 +12,21 @@ public class ConfigWindow
         {
             if (ImGui.BeginTabItem("Colors"))
             {
-                DrawColorPickerOption("Background Color", ref Config.Styles.BackgroundColor);
-                DrawColorPickerOption("Selected Color", ref Config.Styles.SelectedColor);
-                DrawColorPickerOption("Hidden Color", ref Config.Styles.HiddenColor);
-                DrawColorPickerOption("Address Color", ref Config.Styles.AddressColor);
-                DrawColorPickerOption("Index Color", ref Config.Styles.IndexColor);
-                DrawColorPickerOption("Offset Color", ref Config.Styles.OffsetColor);
-                DrawColorPickerOption("VTable Color", ref Config.Styles.VTableColor);
-                DrawColorPickerOption("Hex Value Color", ref Config.Styles.HexValueColor);
-                DrawColorPickerOption("Comment Color", ref Config.Styles.CommentColor);
-                DrawColorPickerOption("Type Color", ref Config.Styles.TypeColor);
-                DrawColorPickerOption("Text Color", ref Config.Styles.TextColor);
-                DrawColorPickerOption("Name Color", ref Config.Styles.NameColor);
-                DrawColorPickerOption("Plugin Info Color", ref Config.Styles.PluginInfoColor);
-                DrawColorPickerOption("Value Color", ref Config.Styles.ValueColor);
+                DrawColorPickerOption("Background Color"u8, ref Config.Styles.BackgroundColor);
+                DrawColorPickerOption("Selected Color"u8, ref Config.Styles.SelectedColor);
+                DrawColorPickerOption("Hovered Color"u8, ref Config.Styles.HoveredColor);
+                DrawColorPickerOption("Hidden Color"u8, ref Config.Styles.HiddenColor);
+                DrawColorPickerOption("Address Color"u8, ref Config.Styles.AddressColor);
+                DrawColorPickerOption("Index Color"u8, ref Config.Styles.IndexColor);
+                DrawColorPickerOption("Offset Color"u8, ref Config.Styles.OffsetColor);
+                DrawColorPickerOption("VTable Color"u8, ref Config.Styles.VTableColor);
+                DrawColorPickerOption("Hex Value Color"u8, ref Config.Styles.HexValueColor);
+                DrawColorPickerOption("Comment Color"u8, ref Config.Styles.CommentColor);
+                DrawColorPickerOption("Type Color"u8, ref Config.Styles.TypeColor);
+                DrawColorPickerOption("Text Color"u8, ref Config.Styles.TextColor);
+                DrawColorPickerOption("Name Color"u8, ref Config.Styles.NameColor);
+                DrawColorPickerOption("Plugin Info Color"u8, ref Config.Styles.PluginInfoColor);
+                DrawColorPickerOption("Value Color"u8, ref Config.Styles.ValueColor);
                 ImGui.EndTabItem();
             }
 
